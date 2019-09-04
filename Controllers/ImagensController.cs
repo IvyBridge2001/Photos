bbe979511c7b88c94eefa2564c74016b4f78fb43
Daniel <33791204+IvyBridge2001@users.noreply.github.com>
2019-09-03 22:15:42 -0300

    Initial Commit

---
+++ Controllers/ImagensController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Photos.Models;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace Photos.Controllers
+{
+
+
+    public class ImagensController : Controller
+    {
+        ImagemDBContext _context;
+        public ImagensController(ImagemDBContext contexto)
+        {
+            _context = contexto;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<int> imagens = _context.Imagens.Select(m => m.Id).ToList();
+            return View(imagens);
+        }
+        [HttpPost]
+        [RequestFormLimits(ValueLengthLimit = int.MaxValue, MultipartBodyLengthLimit = int.MaxValue)]
+        public IActionResult UploadImagem(IList<IFormFile> arquivos)
+        {
+            IFormFile imagemEnviada = arquivos.FirstOrDefault();
+            if (imagemEnviada != null || imagemEnviada.ContentType.ToLower().StartsWith("image/"))
+            {
+                MemoryStream ms = new MemoryStream();
+                imagemEnviada.OpenReadStream().CopyTo(ms);
+                Imagem imagemEntity = new Imagem()
+                {
+                    Nome = imagemEnviada.Name,
+                    Dados = ms.ToArray(),
+                    ContentType = imagemEnviada.ContentType
+                };
+                _context.Imagens.Add(imagemEntity);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public FileStreamResult VerImagem(int id)
+        {
+            Imagem imagem = _context.Imagens.FirstOrDefault(m => m.Id == id);
+            MemoryStream ms = new MemoryStream(imagem.Dados);
+            return new FileStreamResult(ms, imagem.ContentType);
+        }
+    }
+
+}
---
+++ Models/ImagemDBContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Photos.Models
+{
+    public class ImagemDBContext : DbContext
+    {
+        public ImagemDBContext(DbContextOptions<ImagemDBContext> options) : base(options)
+        { }
+        public DbSet<Imagem> Imagens { get; set; }
+    }
+}
